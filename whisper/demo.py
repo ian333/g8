33c1ea9a8c478c85fba1737609898435ee88e4a7
ian333 <sebastianvaz123@hotmail.com>
2023-02-21 20:56:40 -0600

    Se "completo el servicio del modelo whisper de la empresa fantasma
    dsadsa
    
    11/03/1992
    
    Sprint
    Team : G9"

--- whisper/demo.py
+++ whisper/demo.py
@@ -7,3 +7,24 @@ import numpy as np
 from scipy.io.wavfile import read as wav_read
 import io
 import ffmpeg
+
+
+Tarea = "Transcript to Language" #@param ["Transcript to Language", "Translate to English"]
+import numpy as np
+import whisper
+from scipy.io.wavfile import write
+from IPython.display import clear_output
+
+
+def get_audio():
+  display(HTML(AUDIO_HTML))
+  data = eval_js("data")
+  binary = b64decode(data.split(',')[1])
+  
+  process = (ffmpeg
+    .input('pipe:0')
+    .output('pipe:1', format='wav')
+    .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True, quiet=True, overwrite_output=True)
+  )
+  output, err = process.communicate(input=binary)
+  
