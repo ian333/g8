edc6ae6757ebe92a3f0b98303a38f9f77def8a85
ian333 <sebastianvaz123@hotmail.com>
2023-02-21 20:53:27 -0600

    Añadi las librerias
    Signed-off-by: ian333 <sebastianvaz123@hotmail.com>

---
+++ whisper/demo.py
@@ -0,0 +1,9 @@
+#aqui ira el codigo de whisper
+
+from IPython.display import HTML, Audio
+from google.colab.output import eval_js
+from base64 import b64decode
+import numpy as np
+from scipy.io.wavfile import read as wav_read
+import io
+import ffmpeg
