e5fb517c2a775ebeff62876b70e15b61913d3565
ian333 <sebastianvaz123@hotmail.com>
2023-02-21 21:06:03 -0600

    Añadi conflictos para resolverlos despues
    Signed-off-by: ian333 <sebastianvaz123@hotmail.com>

--- whisper/demo.py
+++ whisper/demo.py
@@ -4,12 +4,16 @@ from IPython.display import HTML, Audio
 from google.colab.output import eval_js
 from base64 import b64decode
 import numpy as np
+
+#estas son las librerias para trabajar con archivos .wav
 from scipy.io.wavfile import read as wav_read
 import io
 import ffmpeg
 
 
 Tarea = "Transcript to Language" #@param ["Transcript to Language", "Translate to English"]
+
+#estas son las librerias especiales
 import numpy as np
 import whisper
 from scipy.io.wavfile import write
@@ -23,8 +27,8 @@ def get_audio():
   
   process = (ffmpeg
     .input('pipe:0')
-    .output('pipe:1', format='wav')
-    .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True, quiet=True, overwrite_output=True)
+    .output('pipe:1', format='mp3')
+    .run_async(pipe_stdin=False, pipe_stdout=True, pipe_stderr=True, quiet=True, overwrite_output=True)
   )
   output, err = process.communicate(input=binary)
   
